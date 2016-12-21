61f89ab68778d5c9179e472ff52a50297c609c3c
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2016-12-21 17:27:32 -0300

    Se agregó formulario para edit qe aun no funciona

--- form/editform.php
+++ form/editform.php
@@ -3,51 +3,53 @@ defined('MOODLE_INTERNAL') || die();
 require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
 require_once($CFG->libdir . "/formslib.php");
 
-
-class calendario2_crear_form extends moodleform {
-	public function definition() {
+class calendario2_editar_form extends moodleform{
+	function definition(){
 		global $DB, $CFG;
-
 		$mform = $this->_form;
-
-	//	$user = $this->id.USER;
+		$instance = $this->_customdata;
+		$id=$instance["id"];
+		$evento=$instance["evento"];
+		$descripcion=$instance["descripcion"];
+		$fechaevento=$instance["fechaevento"];
 		
-		
-		$mform->addElement("text", "evento", "evento");
-		$mform->setType( "evento", PARAM_TEXT);
-		$mform->addElement("text", "descripcion", "descripcion");
-		$mform->setType( "descripcion", PARAM_TEXT);
+		$mform->addElement("hidden", "action", "edit");
+		$mform->setType("action", PARAM_TEXT);
+		$mform->addElement("hidden", "id", $id);
+		$mform->setType("id", PARAM_INT);
+		$mform->addElement("text", "evento", $evento);
+		$mform->setType("evento", PARAM_TEXT);
+		$mform->addElement("text", "descripcion", $descripcion);
+		$mform->setType("descripcion", PARAM_TEXT);
 		$mform->addElement("date_selector", "fecha", "fecha");
 		$mform->setType( "fecha", PARAM_INT);
 		
-		$mform->addElement("hidden", "action", "add");
-		$mform->setType("action", PARAM_TEXT);
-		
-		$this->add_action_buttons(true, 'Agregar Evento');
+		$this->add_action_buttons(true);
 	}
-	
+		
 	public function validation($data, $files){
 		$errors = array();
-		
+	
 		$evento = $data["evento"];
 		$descripcion = $data["descripcion"];
 		$fecha = $data["fecha"];
-		
+	
 		if(empty($evento)){
 			$errors["evento"] = "Debe ponerle nombre a su evento";
 		}
-		
+	
 		if(empty($descripcion)){
 			$errors["descripcion"] = "De que se trata su evento?";
 		}
-		
+	
 		$today = time();
 		if ($today > $fecha){
 			$errors["fecha"] = "Debe seleccionar una fecha";
-			
+				
 		}
-		
+	
 		return $errors;
+	
 		
 	}
 }
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once(dirname(__FILE__) . '/../../config.php');
 require_once ($CFG->dirroot . "/local/calendario2/form/form.php");
+require_once ($CFG->dirroot . "/local/calendario2/form/editform.php");
 global $PAGE, $CFG, $OUTPUT, $DB, $USER;
 
 require_login();
@@ -20,6 +21,7 @@ $formulario = new calendario2_crear_form();
 echo $OUTPUT->header();
 
 $action = optional_param("action", "viewevent", PARAM_TEXT);
+$edition = optional_param("id", null, PARAM_INT);
 
 if ($action=="add"){
 
@@ -27,7 +29,7 @@ if ($action=="add"){
 		$action = "viewevent";
 	}
 	else if ($fromform = $formulario->get_data()) {
-		echo "hay datos";
+		echo "hay datos <br>";
 		
 		$big_event = new stdClass();
 		$big_event->evento = $fromform->evento;
@@ -40,13 +42,99 @@ if ($action=="add"){
 		$action = "viewevent";
 	}
 }
+//**********************************************************************************************
+//cosa para editar
+$tabla = new html_table();
+
+
+$query = "SELECT * from mdl_calendario2_evento WHERE iduser =?";
+$ndeeventos = $DB->get_records_sql($query, array ("iduser"=>$userid));
+$contadordeeventos = count($ndeeventos);
+if($contadordeeventos>0){
+	for($i=1; $i<=$contadordeeventos; $i++){
+		$urlevent = new moodle_url("/local/calendario2/index.php", array(
+				"action" => "edit",
+				"edition" => $ndeeventos[$i]->id)); //error: la wea no cambia el valor de edition
+
+		$editeventicon = new pix_icon("i/edit", "Editar");
+		$editeventiconaction = $OUTPUT->action_icon($urlevent, $editeventicon);
+		
+		//Borrar
+		//*************************************
+		$edition = 1;
+		//Prueba para ver qe pasa si lo de arriba funcionara
+		echo $edition;
+		//*************************************
+		//Borrar
+		$tabla->data[] = array(
+				$ndeeventos[$i]->evento,
+				$ndeeventos[$i]->descripcion,
+				date("d-m-Y", (float)$ndeeventos[$i]->fechaevento),
+				$editeventiconaction
+		);
+//		echo "hola";
+
+	}
+}
+
+if ($action == "edit"){
+	
+	if ($edition == null){
+		//checks if there is something selected to be edited
+		echo "No hay nada seleccionado para editar";
+		$action = "viewevent";
+	}
+	else{
+		$newquery = "SELECT * from mdl_calendario2_evento WHERE iduser =? AND id = $edition";
+		if($editar_evento = $DB->get_records_sql($newquery, array ("iduser"=>$userid, "id"=>$edition))){
+						
+			$editform = new calendario2_editar_form(null, array("id"=>$edition,
+					"evento"=>$editar_evento[1]->evento,
+					"descripcion"=>$editar_evento[1]->descripcion,
+					"fechaevento"=>$editar_evento[1]->fechaevento));
+			
+			$editform->display();
+			//Error: no rellena los datos; sigue con los sigtes ifs al tiro
+			
+			
+			if ($editform->is_cancelled()){
+				$cancel = new moodle_url("/local/calendario2/index.php", array(
+							"action" => "viewevent"));
+					redirect($cancel);
+			}
+			else if($editform->get_data()){
+				$editado = new stdClass();
+				$editado->id = $edition;
+				$editado->evento = $editform->get_data()->evento;
+				$editado->descripcion = $editform->get_data()->descripcion;
+				$editado->fechaevento = $editform->get_data()->fechaevento;
+				
+				$DB->update_record("calendario2_evento", $editado);
+				
+				$gobackurl = new moodle_url("/local/calendario2/index.php", array(
+						"action" => "viewevent"));
+				redirect($gobackurl);
+			}
+			else{
+				echo "Error Fatal!!!";
+				$gobackurl = new moodle_url("/local/calendario2/index.php", array(
+						"action" => "viewevent"));
+				redirect($gobackurl);
+			}
+		}
+		
+	}
+}
+//Fin cosa para editar
+//**********************************************************************************************	
 
 if ($action == "viewevent"){
 	$time = time();
 	//fechaevento > $time AND 
 	$sql = "SELECT * from mdl_calendario2_evento WHERE iduser =?";
 	$eventos = $DB->get_records_sql($sql, array ("iduser"=>$userid));
-	$contador = count($eventos); 
+	$contador = count($eventos);
+	//echo $contador;
 	//echo $eventos[1]->evento;
 	$string = "";
 	$botonurl = new moodle_url("/local/calendario2/index.php", array("action" => "add"));
@@ -67,9 +155,16 @@ if ($action == "add"){
 	$formulario->display();
 }
 
+if ($action == "edit"){
+	//Error: no llega a este edit para llenar el formulario
+	$editform->display();
+}
+
 if ($action == "viewevent"){
-	echo $string;
 	echo $OUTPUT->single_button($botonurl,"Agregar Evento");
+	echo html_writer::table($tabla);
+	//echo $string;
+	
 }
 
 echo $OUTPUT->footer();
