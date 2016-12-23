04af5771193e76f50c9fb5698ab378c4b04368aa
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2016-12-23 12:04:22 -0300

    It works. It has some echos which need to be changed for something... better

--- index.php
+++ index.php
@@ -16,94 +16,75 @@ $context = context_system::instance();
 $PAGE->set_context($context);
 $PAGE->set_url($url);
 $PAGE->set_pagelayout('standard');
-//$PAGE->set_title($title);
-//$PAGE->set_heading($title);
 
 $formulario = new calendario2_crear_form();
 
 
-
-
+//**********************************************************************************************
+//Add a new event
 if ($action=="add"){
-
 	if ($formulario->is_cancelled()) {
 		$action = "viewevent";
 	}
 	else if ($fromform = $formulario->get_data()) {
-		echo "hay datos <br>";
-		
+		//Takes the data from the form
 		$big_event = new stdClass();
 		$big_event->evento = $fromform->evento;
 		$big_event->descripcion = $fromform->descripcion;
 		$big_event->fechacreacion = time();
 		$big_event->fechaevento = $fromform->fecha;
 		$big_event->iduser = $userid;
-		//echo $big_event->userid;
 		$insert_action = $DB->insert_record("calendario2_evento", $big_event, $returnid=true, $bulk=false);
+		//Inserts the data into the DB
 		$action = "viewevent";
+		//Let's go see the new event among the rest
 	}
 }
+//End of adding a new event
 //**********************************************************************************************
-//cosa para editar
+//Edit
 if ($action == "edit"){
-	
 	if ($edition == null){
-		//checks if there is something selected to be edited
 		echo "No hay nada seleccionado para editar";
 		$action = "viewevent";
 	}
 	else{
+		//$edition has the id of the selected event
 		$newquery = "SELECT * from mdl_calendario2_evento WHERE iduser =? AND id = $edition";
 		if($editar_evento = $DB->get_records_sql($newquery, array ("iduser"=>$userid, "id"=>$edition))){
-			
-//			echo count($editar_evento);  No entiendo por qe funciona, pero funciona...
-			
+			//if there is an event with such id and belongs to the user
 			$editform = new calendario2_editar_form(null, array("edition"=>$edition,
 					"evento"=>$editar_evento[$edition]->evento,
 					"descripcion"=>$editar_evento[$edition]->descripcion,
 					"fechaevento"=>$editar_evento[$edition]->fechaevento));
-			
+			//Not really sure why this works... ¯\_(ツ)_/¯
 			$defaultdata = new stdClass();
 			$defaultdata->evento = $editar_evento[$edition]->evento;
 			$defaultdata->descripcion = $editar_evento[$edition]->descripcion;
 			$defaultdata->fechaevento = $editar_evento[$edition]->fechaevento;
 			$editform->set_data($defaultdata);
+			//Fills the form with the data from the DB
+			//NOTE: Fills the date with the current date, not the one from the event
 			
 			if ($editform->is_cancelled()){
-/*				$cancel = new moodle_url("/local/calendario2/index.php", array(
-							"action" => "viewevent"));
-				redirect($cancel);*/
 				$action = "viewevent";
 			}
 			else if($editar = $editform->get_data()){
-				echo "<br>";
 				$editado = new stdClass();
 				$editado->id = $edition;
 				$editado->evento = $editar->evento;
 				$editado->descripcion = $editar->descripcion;
 				$editado->fechaevento = $editar->fecha;
-				
+				//Takes the new data and updates it in the DB
 				$DB->update_record("calendario2_evento", $editado);
 				$action = "viewevent";
 			}
 		}
 	}
 }
-//Fin cosa para editar
+//End of Edit
 //**********************************************************************************************	
-//Inicio cosa para borrar
-
-if ($action == "predelete"){
-	echo "Esta completa y absolutamente seguro que quiere borrar permanentemente el evento seleccionado?";
-	$botondeletey = new moodle_url("/local/calendario2/index.php", array("action" => "delete", "edition"=>$edition));
-	$botondeleten = new moodle_url("/local/calendario2/index.php", array("action" => "viewevent"));
-	echo $OUTPUT->single_button($botondeletey,"Si");
-	echo $OUTPUT->single_button($botondeleten,"No");
-}
-
-
-
-
+//"Delete"
 if ($action == "delete"){
 	if ($edition == null){
 		echo "No hay nada seleccionado para borrar";
@@ -115,40 +96,43 @@ if ($action == "delete"){
 		$deleter->fechacreacion = 0;
 		
 		$DB->update_record("calendario2_evento", $deleter);
+		//Update selected event, if fechacreacion is 0, then it won't be shown anymore
+		//But it will remain in the DB
 		$action = "viewevent";
 		echo "Evento borrado satisfactoriamente";
 	}
-
-
 }
-//Fin cosa para borrar
+//End "delete"
 //**********************************************************************************************
-
+//View events
 if ($action == "viewevent"){
 	$tabla = new html_table();
 
-
 	$query = "SELECT * from mdl_calendario2_evento WHERE iduser =? AND fechacreacion != 0";
+	//Get all events from user which haven't been "deleted"
 	$ndeeventos = $DB->get_records_sql($query, array ("iduser"=>$userid));
 	$contadordeeventos = count($ndeeventos);
 	$botonurl = new moodle_url("/local/calendario2/index.php", array("action" => "add"));
 	if($contadordeeventos>0){
+		//If there are events in the DB from this user which have not been deleted...
 		foreach($ndeeventos as $ev){
+			//Add a button for each event for editing or deleting
 			$urlevent = new moodle_url("/local/calendario2/index.php", array(
 					"action" => "edit",
 					"edition" => $ev->id,
 			));
 			
 			$urldelete = new moodle_url("/local/calendario2/index.php", array(
-					"action" => "predelete", //aqui cambie la wea
+					"action" => "delete",
 					"edition" => $ev->id,
 			));
 			
 			$editeventicon = new pix_icon("i/edit", "Editar");
 			$editeventiconaction = $OUTPUT->action_icon($urlevent, $editeventicon);
 
-			$deleteeventicon = new pix_icon("i/delete", "Borrar");
-			$deleteeventiconaction = $OUTPUT->action_icon($urldelete, $deleteeventicon);
+			$deleteeventicon = new pix_icon("t/delete", "Borrar");
+			$deleteeventiconaction = $OUTPUT->action_icon($urldelete, $deleteeventicon,
+					new confirm_action("Esta completamente seguro de que quiere borrar este evento?"));
 			
 			
 			$tabla->data[] = array(
@@ -157,10 +141,12 @@ if ($action == "viewevent"){
 					date("d-m-Y", (float)$ev->fechaevento),
 					$editeventiconaction,
 					$deleteeventiconaction
-			);
+			); //Show the retrieved data into a table for viewing
 		}
 	}
 }
+//End of view
+//**********************************************************************************************
 
 echo $OUTPUT->header();
 
